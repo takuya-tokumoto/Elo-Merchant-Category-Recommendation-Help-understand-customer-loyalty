19709daf0fb4ccb76339a8aa6a5e614a156542b0
takuya-tokumoto <takuya.0810@hotmail.co.jp>
2020-08-02 11:01:57 +0900

    run

--- run.py
+++ run.py
@@ -1,84 +1,171 @@
-import pandas as pd
-import datetime
-import logging
-from sklearn.model_selection import KFold
-import argparse
-import json
+# part of 2nd place solution: lightgbm model with private score 0.29124 and public lb score 0.28555
+
+import lightgbm as lgbm
+from scipy import sparse as ssp
+from sklearn.model_selection import StratifiedKFold
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
-from utils import load_datasets, load_target
-from logs.logger import log_best
-from models.lgbm import train_and_predict
+def Gini(y_true, y_pred):
+    # check and get number of samples
+    assert y_true.shape == y_pred.shape
+    n_samples = y_true.shape[0]
+
+    # sort rows on prediction column
+    # (from largest to smallest)
+    arr = np.array([y_true, y_pred]).transpose()
+    true_order = arr[arr[:, 0].argsort()][::-1, 0]
+    pred_order = arr[arr[:, 1].argsort()][::-1, 0]
+
+    # get Lorenz curves
+    L_true = np.cumsum(true_order) * 1. / np.sum(true_order)
+    L_pred = np.cumsum(pred_order) * 1. / np.sum(pred_order)
+    L_ones = np.linspace(1 / n_samples, 1, n_samples)
+
+    # get Gini coefficients (area between curves)
+    G_true = np.sum(L_ones - L_true)
+    G_pred = np.sum(L_ones - L_pred)
+
+    # normalize to true Gini coefficient
+    return G_pred * 1. / G_true
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', default='./configs/default.json')
-options = parser.parse_args()
-config = json.load(open(options.config))
+cv_only = True
+save_cv = True
+full_train = False
 
-now = datetime.datetime.now()
-logging.basicConfig(
-    filename='./logs/log_{0:%Y%m%d%H%M%S}.log'.format(now), level=logging.DEBUG
-)
-logging.debug('./logs/log_{0:%Y%m%d%H%M%S}.log'.format(now))
 
-feats = config['features']
-logging.debug(feats)
+def evalerror(preds, dtrain):
+    labels = dtrain.get_label()
+    return 'gini', Gini(labels, preds), True
 
-target_name = config['target_name']
 
-X_train_all, X_test = load_datasets(feats)
-y_train_all = load_target(target_name)
-logging.debug(X_train_all.shape)
+path = "data/input/"
 
-y_preds = []
-models = []
+train = pd.read_csv(path+'train.csv')
+train_label = train['target']
+train_id = train['id']
+test = pd.read_csv(path+'test.csv')
+test_id = test['id']
 
-lgbm_params = config['lgbm_params']
+NFOLDS = 5
+kfold = StratifiedKFold(n_splits=NFOLDS, shuffle=True, random_state=218)
 
-kf = KFold(n_splits=3, random_state=0)
-for train_index, valid_index in kf.split(X_train_all):
-    X_train, X_valid = (
-        X_train_all.iloc[train_index, :], X_train_all.iloc[valid_index, :]
-    )
-    y_train, y_valid = y_train_all[train_index], y_train_all[valid_index]
-
-    # lgbmの実行
-    y_pred, model = train_and_predict(
-        X_train, X_valid, y_train, y_valid, X_test, lgbm_params
-    )
-
-    # 結果の保存
-    y_preds.append(y_pred)
-    models.append(model)
-
-    # スコア
-    log_best(model, config['loss'])
-
-# CVスコア
-scores = [
-    m.best_score['valid_0'][config['loss']] for m in models
+y = train['target'].values
+drop_feature = [
+    'id',
+    'target'
 ]
-score = sum(scores) / len(scores)
-print('===CV scores===')
-print(scores)
-print(score)
-logging.debug('===CV scores===')
-logging.debug(scores)
-logging.debug(score)
 
-# submitファイルの作成
-ID_name = config['ID_name']
-sub = pd.DataFrame(pd.read_csv('./data/input/test.csv')[ID_name])
+X = train.drop(drop_feature,axis=1)
+feature_names = X.columns.tolist()
+cat_features = [c for c in feature_names if ('cat' in c and 'count' not in c)]
+num_features = [c for c in feature_names if ('cat' not in c and 'calc' not in c)]
 
-y_sub = sum(y_preds) / len(y_preds)
+train['missing'] = (train==-1).sum(axis=1).astype(float)
+test['missing'] = (test==-1).sum(axis=1).astype(float)
+num_features.append('missing')
 
-if y_sub.shape[1] > 1:
-    y_sub = np.argmax(y_sub, axis=1)
+for c in cat_features:
+    le = LabelEncoder()
+    le.fit(train[c])
+    train[c] = le.transform(train[c])
+    test[c] = le.transform(test[c])
 
-sub[target_name] = y_sub
+enc = OneHotEncoder(categories='auto')
+enc.fit(train[cat_features])
+X_cat = enc.transform(train[cat_features])
+X_t_cat = enc.transform(test[cat_features])
 
-sub.to_csv(
-    './data/output/sub_{0:%Y%m%d%H%M%S}_{1}.csv'.format(now, score),
-    index=False
-)
+ind_features = [c for c in feature_names if 'ind' in c]
+count=0
+for c in ind_features:
+    if count==0:
+        train['new_ind'] = train[c].astype(str)+'_'
+        test['new_ind'] = test[c].astype(str)+'_'
+        count+=1
+    else:
+        train['new_ind'] += train[c].astype(str)+'_'
+        test['new_ind'] += test[c].astype(str)+'_'
+
+cat_count_features = []
+for c in cat_features+['new_ind']:
+    d = pd.concat([train[c],test[c]]).value_counts().to_dict()
+    train['%s_count'%c] = train[c].apply(lambda x:d.get(x,0))
+    test['%s_count'%c] = test[c].apply(lambda x:d.get(x,0))
+    cat_count_features.append('%s_count'%c)
+
+train_list = [train[num_features+cat_count_features].values,X_cat,]
+test_list = [test[num_features+cat_count_features].values,X_t_cat,]
+
+X = ssp.hstack(train_list).tocsr()
+X_test = ssp.hstack(test_list).tocsr()
+
+learning_rate = 0.1
+num_leaves = 15
+min_data_in_leaf = 2000
+feature_fraction = 0.6
+num_boost_round = 10000
+params = {"objective": "binary",
+          "boosting_type": "gbdt",
+          "learning_rate": learning_rate,
+          "num_leaves": num_leaves,
+           "max_bin": 256,
+          "feature_fraction": feature_fraction,
+          "verbosity": 0,
+          "drop_rate": 0.1,
+          "is_unbalance": False,
+          "max_drop": 50,
+          "min_child_samples": 10,
+          "min_child_weight": 150,
+          "min_split_gain": 0,
+          "subsample": 0.9
+          }
+
+x_score = []
+final_cv_train = np.zeros(len(train_label))
+final_cv_pred = np.zeros(len(test_id))
+for s in range(16):
+    cv_train = np.zeros(len(train_label))
+    cv_pred = np.zeros(len(test_id))
+
+    params['seed'] = s
+
+    if cv_only:
+        kf = kfold.split(X, train_label)
+
+        best_trees = []
+        fold_scores = []
+
+        for i, (train_fold, validate) in enumerate(kf):
+            X_train, X_validate, label_train, label_validate = \
+                X[train_fold, :], X[validate, :], train_label[train_fold], train_label[validate]
+            dtrain = lgbm.Dataset(X_train, label_train)
+            dvalid = lgbm.Dataset(X_validate, label_validate, reference=dtrain)
+            bst = lgbm.train(params, dtrain, num_boost_round, valid_sets=dvalid, feval=evalerror, verbose_eval=100,
+                            early_stopping_rounds=100, )
+            best_trees.append(bst.best_iteration)
+            cv_pred += bst.predict(X_test, num_iteration=bst.best_iteration)
+            cv_train[validate] += bst.predict(X_validate)
+
+            score = Gini(label_validate, cv_train[validate])
+            print(score)
+            fold_scores.append(score)
+
+        cv_pred /= NFOLDS
+        final_cv_train += cv_train
+        final_cv_pred += cv_pred
+
+        print("cv score:")
+        print(Gini(train_label, cv_train))
+        print("current score:", Gini(train_label, final_cv_train / (s + 1.)), s+1)
+        print(fold_scores)
+        print(best_trees, np.mean(best_trees))
+
+        x_score.append(Gini(train_label, cv_train))
+
+print(x_score)
+pd.DataFrame({'id': test_id, 'target': final_cv_pred / 16.}).to_csv('data/output/lgbm3_pred_avg.csv', index=False)
+pd.DataFrame({'id': train_id, 'target': final_cv_train / 16.}).to_csv('data/output/lgbm3_cv_avg.csv', index=False)
